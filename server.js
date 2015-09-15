0130c833bd14591af5c000c82d2b109d9989d063
Ken Kohlmann <me163@mac.com>
2015-09-15 10:46:25 -0700

    modify port to cater to heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('./config/express');
 var app = express();
-app.listen(80);
+var port = process.env.PORT || 3000;
+app.listen(port);
 module.exports = app;
-console.log('Server running at http://localhost:80/');
+console.log('Server running at http://localhost:'+port);
